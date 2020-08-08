46b8d3507b2334807a56d75d55e99e777321be40
niukang <xiaomajiete@gmail.com>
2020-08-08 13:35:36 +0800

    增加插入排序

--- src/com/sort/Insertion_Sort.java
+++ src/com/sort/Insertion_Sort.java
@@ -1,11 +1,34 @@
 package com.sort;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * @author niu
  * @version 1.0
  * @className Insertion_Sort
- * @description TODO
+ * @description 插入排序
  * @date 2020/8/8 12:46 下午
  */
 public class Insertion_Sort {
+
+    public static void sort(int[] arrays) {
+        for (int j = 1; j < arrays.length; j++) {
+            int key = arrays[j];
+            int i = j - 1;
+            while (i >= 0 && key < arrays[i]) {
+                arrays[i + 1] = arrays[i];
+                i--;
+            }
+            arrays[i + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arrays = new int[]{1, 3, 66, 9, 88, 22};
+        sort(arrays);
+        for (int i : arrays) {
+            System.out.println(i);
+        }
+    }
 }
